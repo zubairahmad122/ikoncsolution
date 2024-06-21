6947ded3fe0f8b43091ce5c3167be8d764f20f1d
zubair122 <zubair520548ahm@gmail.com>
2024-06-21 04:31:22 -0700

    change anim

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 // Initialize Locomotive Scroll
 gsap.registerPlugin()
 function init() {
+  gsap.registerPlugin(ScrollTrigger);
+
   const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".main"),
-    smooth: true,
+      el: document.querySelector(".main"),
+      smooth: true,
+
+      // for tablet smooth
+      tablet: { smooth: true },
+
+      // for mobile
+      smartphone: { smooth: true }
   });
-  
   locoScroll.on("scroll", ScrollTrigger.update);
 
   ScrollTrigger.scrollerProxy(".main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed",
+      scrollTop(value) {
+          return arguments.length
+              ? locoScroll.scrollTo(value, 0, 0)
+              : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+          return {
+              top: 0,
+              left: 0,
+              width: window.innerWidth,
+              height: window.innerHeight
+          };
+      }
   });
 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
   ScrollTrigger.refresh();
 }
 
